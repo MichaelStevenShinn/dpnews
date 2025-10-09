f892f3c0b699fe9174df04f4ee384771d2a960b5
MichaelStevenShinn <michaelstevenshinn@gmail.com>
2025-10-09 00:25:15 -0700

    all i need is background media for CC, and add more videos

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-const header = document.querySelector("header");
+const headerTop = document.querySelector(".header-top");
+const headerBottom = document.querySelector(".header-bottom");
+let lastScroll = 0;
+let scrollUpCheck = 0;
+let i = 0;
 
 window.addEventListener("scroll", () => {
-  const pos = window.scrollY;
+  const currentScroll = window.scrollY;
 
-  if (pos > 30) {
-    // user scrolled down — hide header
-    header.classList.add("hidden");
+  if (lastScroll < currentScroll) {
+    scrollUpCheck = currentScroll;
+  }
+  lastScroll = currentScroll;
+
+  if (scrollUpCheck - currentScroll < 30 && currentScroll > 30) { 
+    headerBottom.classList.add("hidden");
+    if (!headerTop.classList.contains("hidden")) {
+      i = 0;
+      // console.log("A");
+    }
+  } else { 
+    headerBottom.classList.remove("hidden");
+    i++;
+  }
+
+  // Fade top header
+  if (currentScroll > 30) {
+    // headerTop.classList.add("hidden");
+    headerTop.classList.add('fade-out'); // enable easing
+  requestAnimationFrame(() => headerTop.classList.add('hidden'))
   } else {
-    // user scrolled up — show header
-    header.classList.remove("hidden");
+    // headerTop.classList.remove("hidden");
+    headerTop.classList.remove('hidden'); // show instantly
+    headerTop.classList.remove('fade-out'); // disable easing again
+  }
+
+  // Slide up bottom header when top header hidden and visible long enough
+  if (headerTop.classList.contains("hidden") && i >= 4) {
+    requestAnimationFrame(() => headerBottom.classList.add("go-top"));
+  } else {
+    requestAnimationFrame(() => headerBottom.classList.remove("go-top"));
   }
 });
-
-
